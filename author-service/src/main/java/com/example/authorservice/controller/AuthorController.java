78dcaa7a849fab3d9d0e7eb5724e6b0cfe8c1d31
joao fernandes <joalo.fernandes@cognizant.com>
2022-11-11 16:17:34 +0000

    Initial commit

---
+++ author-service/src/main/java/com/example/authorservice/controller/AuthorController.java
@@ -0,0 +1,14 @@
+package com.example.authorservice.controller;
+
+import com.example.authorservice.controller.dto.AuthorDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+
+    @GetMapping("/author/{authorId}")
+    public AuthorDto getAuthor() {
+        return AuthorDto.builder().name("Joao").age(20).build();
+    }
+}
