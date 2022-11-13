4dbc8d7d4e372fb4526a120f2e67dac5cbf50601
joao fernandes <joalo.fernandes@cognizant.com>
2022-11-13 17:08:01 +0000

    added the spring cloud gateway

--- author-service/src/main/java/com/example/authorservice/controller/AuthorController.java
+++ author-service/src/main/java/com/example/authorservice/controller/AuthorController.java
@@ -3,11 +3,13 @@ package com.example.authorservice.controller;
 import com.example.authorservice.controller.dto.AuthorDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/author")
 public class AuthorController {
-    @GetMapping("/author/{authorId}")
+    @GetMapping("/{authorId}")
     public AuthorDto getAuthor(@PathVariable("authorId") int authorId) {
         return AuthorDto.builder().name("Joao").age(20).build();
     }
